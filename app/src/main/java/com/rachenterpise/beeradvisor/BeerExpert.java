91ffc35c75fad810b22cb43895f104a608f6e47f
chiboyap <60558376+chiboyap@users.noreply.github.com>
2020-10-25 23:28:40 +0100

    Apk installation file updated and the few statements added to the BeerExpert.java file to use a switch statement instead of the traditional if statement

--- app/src/main/java/com/rachenterpise/beeradvisor/BeerExpert.java
+++ app/src/main/java/com/rachenterpise/beeradvisor/BeerExpert.java
@@ -12,26 +12,35 @@ public class BeerExpert {
     List<String> getBrands(String color){
         List<String> brands = new ArrayList<>();
 
-        if(color.equals("amber")){
-            brands.add("Jack Amber");
-            brands.add("Red Moose");
-            brands.add("Star");
-            brands.add("Guilder");
-            brands.add("Legend");
-            brands.add("Hero");
-            brands.add("Life");
-            brands.add("Heineken");
-            brands.add("Harp");
-            brands.add("33");
-        }else{
-            brands.add("Jail Pale Ale");
-            brands.add("Gout Stout");
-            brands.add("Guinness Stout");
-            brands.add("Orijin");
-            brands.add("Smirnoff Ice");
-            brands.add("Smirnoff Chocolate");
-            brands.add("Budweiser");
-            brands.add("Guinness Stout Smooth");
+        switch (color) {
+            case "amber":
+                brands.add("Jack Amber");
+                brands.add("Red Moose");
+                brands.add("Star");
+                brands.add("Guilder");
+                brands.add("Legend");
+                brands.add("Hero");
+                brands.add("Life");
+                brands.add("Heineken");
+                brands.add("Harp");
+                brands.add("33");
+                break;
+            case "light":
+                brands.add("Jail Pale Ale");
+                brands.add("Smirnoff Ice");
+
+                break;
+            case "dark":
+                brands.add("Barely Malt");
+                brands.add("Gout Stout");
+                brands.add("Guinness Stout");
+                brands.add("Orijin");
+                brands.add("Budweiser");
+                break;
+            default:
+                brands.add("Smirnoff Chocolate");
+                brands.add("Guinness Stout Smooth");
+                break;
         }
         return brands;
     }
